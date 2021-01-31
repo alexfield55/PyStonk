b05b6301f00c6a4a19d4fc8806c2aa522670112a
sam1010k <Kahan122922@gmail.com>
2021-01-31 09:47:56 -0800

    Update pystonk.py

--- pystonk.py
+++ pystonk.py
@@ -1,41 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 30 08:58:06 2021
-
-@author: jimb
-"""
-
+from ISStreamer.Streamer import Streamer
+import time
 import requests
-import pandas as pd
-import plotly.express as px
-import json
 
-data_type="comment"     # give me comments, use "submission" to publish something
-query="monday"          # Add your query
-duration="3d"          # Select the timeframe. Epoch value or Integer + "s,m,h,d" (i.e. "second", "minute", "hour", "day")
+#diamondhands = str(u"💎")
+
+data_type="comment"  
+querylist = ["monday", "monday&buy", "monday&hold", "monday&sell",
+             "yolo", "GME", "AMC", "this is the way", "like the stock",
+             "not selling", "melvin", "to the moon" ]        
+after="1611878400"       
 subreddit="wallstreetbets"
-aggs="created_utc"        #"author", "link_id", "created_utc", "subreddit"
+metadata=[]
 
+marketopens = 1612161000
+
+#this is for that bootleg front end I am using, if we build our own we can get rid of this
+ACCESS_KEY = "ACCESS_KEY"
+BUCKET_KEY = "BUCKET_KEY"
+BUCKET_NAME = "Wall Street Bets Comment Counter"
+
+#function that pulls the data
 def get_pushshift_data(data_type, **kwargs):
-    """
-    Gets data from the pushshift api.
- 
-    data_type can be 'comment' or 'submission'
-    The rest of the args are interpreted as payload.
- 
-    Read more: https://github.com/pushshift/api
-    """
- 
     base_url = f"https://api.pushshift.io/reddit/search/{data_type}/"
     payload = kwargs
     request = requests.get(base_url, params=payload)
     return request.json()
 
-data = get_pushshift_data(data_type=data_type,
-                          q=query,
-                          after=duration,
-                          subreddit=subreddit,
-                          aggs=aggs).get("data")
+# create a Streamer instance, used for the front end
+streamer = Streamer(bucket_name=BUCKET_NAME, bucket_key=BUCKET_KEY, access_key=ACCESS_KEY)
 
-df = pd.DataFrame.from_records(data)[["body"]]
-print(df)
+now = time.time()
+
+#while loop to stop at market open in epoch utc
+while now < marketopens:
+    for i, item in enumerate(querylist):
+        #something going on with the next two lines, was working fine last night now is crapping
+        #wrapped it in a try/except but still doesnt really help...
+        try:
+            metadata.append(get_pushshift_data(data_type=data_type, q=item, after=after, subreddit=subreddit, metadata=True).get("metadata"))
+            streamer.log(f'{item}', metadata[i].get("total_results"))
+        except:
+            pass
+        
+    metadata.clear()
+    now = time.time()
+streamer.flush()
